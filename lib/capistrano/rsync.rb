fb9ab6ec41b871fa017e9055c88b6aeabede5611
Andri Möll <andri@dot.ee>
2013-09-01 03:16:57 +0300

    Default to [] when rsync_options not set.

--- lib/capistrano/rsync.rb
+++ lib/capistrano/rsync.rb
@@ -2,6 +2,7 @@ require File.expand_path("../rsync/version", __FILE__)
 load File.expand_path("../tasks/rsync.rake", __FILE__)
 
 set :rsync_stage, "tmp/deploy"
+set :rsync_options, []
 
 module Capistrano
   module Rsync
