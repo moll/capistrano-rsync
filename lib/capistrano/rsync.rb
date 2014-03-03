2dc9008077ca4ecbc50fceb9ef8b89a2f8ac0a81
Samuel Reed <sam@tixelated.com>
2014-03-03 15:46:21 +0800

    Move usage inside SSHKit so logging shows up as expected.

--- lib/capistrano/rsync.rb
+++ lib/capistrano/rsync.rb
@@ -32,15 +32,17 @@ Rake::Task["deploy:updating"].enhance ["rsync:hook_scm"]
 
 desc "Stage and rsync to the server (or its cache)."
 task :rsync => %w[rsync:stage] do
-  roles(:all).each do |role|
-    user = role.user + "@" if !role.user.nil?
+  on roles(:all), in: :parallel do |host|
+    user = host.user + "@" if !host.user.nil?
 
-    rsync = %w[rsync]
-    rsync.concat fetch(:rsync_options)
-    rsync << fetch(:rsync_stage) + "/"
-    rsync << "#{user}#{role.hostname}:#{rsync_cache.call || release_path}"
+    run_locally do
+      rsynccmd = []
+      rsynccmd.concat(fetch(:rsync_options))
+      rsynccmd << fetch(:rsync_stage) + "/"
+      rsynccmd << "#{user}#{host.hostname}:#{rsync_cache.call || release_path}"
+      execute :rsync, *rsynccmd
+    end
 
-    Kernel.system *rsync
   end
 end
 
@@ -62,20 +64,18 @@ namespace :rsync do
   task :create_stage do
     next if File.directory?(fetch(:rsync_stage))
 
-    clone = %W[git clone]
-    clone << fetch(:repo_url, ".")
-    clone << fetch(:rsync_stage)
-    Kernel.system *clone
+    run_locally do
+      execute :git, "clone #{fetch(:repo_url, '.')} #{fetch(:rsync_stage)}";
+    end
   end
 
   desc "Stage the repository in a local directory."
   task :stage => %w[create_stage] do
-    Dir.chdir fetch(:rsync_stage) do
-      update = %W[git fetch --quiet --all --prune]
-      Kernel.system *update
-
-      checkout = %W[git reset --hard origin/#{fetch(:branch)}]
-      Kernel.system *checkout
+    run_locally do
+      within fetch(:rsync_stage) do
+        execute :git, "fetch --quiet --all --prune"
+        execute :git, "reset --hard origin/#{fetch(:branch)}"
+      end
     end
   end
 
